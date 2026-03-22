19c7d9f54e02bb04f417101fbf44f33fb22b0823
Poornima c <poornima2005c@gmail.com>
2026-03-22 10:16:34 +0530

    Add Student class with basic attributes and methods

---
+++ Student.java
@@ -0,0 +1,40 @@
+package StudentMngmentSystem;
+
+public class Student {
+	private int id;
+	private String name;
+	private int age;
+	Student(int id,String name,int age){
+		this.id=id;
+		this.name=name;
+		this.age=age;
+	}
+	public int getId() {
+		return id;
+	}
+	/*public void setId(int id) {
+		this.id=id;
+	}*/
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		if(age>0) {
+		this.age=age;
+	}
+		else {
+			System.out.println("Invalid age");
+		}
+	}
+	@Override
+	public String toString() {
+		return "Id: "+this.id+" Name: "+this.name+ " Age: "+this.age;
+	}
+	
+}
